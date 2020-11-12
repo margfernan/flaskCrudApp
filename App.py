86bb5b9092fbfc6ff6e4e31a9f09a65fc5bc8fc3
Martin Fernandez <Martin.Fernandez@endava.net>
2020-11-12 17:50:41 -0300

    Mi primer commit

---
+++ App.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'courier'
+app.config['MYSQL_DB'] = 'crudapplication'
+
+mysql = MySQL(app)
+
+
+@app.route('/')
+def Index():
+    return render_template('index.html')
+
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+    try:
+        cur = mysql.connection.cursor()
+        # cur.execute("INSERT INTO student (name, email, phone) VALUES ('naeee', 'fgfd@test.com', 'phee')")
+        cur.execute("INSERT INTO student (name, email, phone) VALUES (%s, %s, %s)", (name, email, phone))
+        mysql.connection.commit()
+        return redirect(url_for('Index'))
+    except Exception as e:
+        print("Problem inserting into db: " + str(e))
+
+        return redirect(url_for('Index'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
